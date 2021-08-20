3e69a789255d77be7ce226892e9ce6a99e34a2c9
Vuyani2 <vuyanikunelisi@gmail.com>
2021-08-20 11:42:17 +0200

    changes

--- JS/cards.js
+++ JS/cards.js
@@ -1,5 +1,3 @@
-let cartProducts = [];
-
 fetch("https://lit-beach-56409.herokuapp.com/sort-product/", {
   method: "GET",
 })
@@ -52,44 +50,92 @@ function addToCart(id) {
     .then((data) => {
       // let products = JSON.parse(localStorage.getItem("products"));
       let products = data.data;
+      console.log(products);
       let cartProduct = products.find((product) => {
         return product[0] == id;
       });
 
+      console.log(cartProduct);
+      let cartProducts =
+        JSON.parse(localStorage.getItem("cart products")) || [];
       cartProducts.push(cartProduct);
-      localStorage.productsOnCart = cartProducts;
-      //
-      // localStorage.ProductsOnCart = cartProduct;
-      let cartContainer = document.querySelector(".list-group");
-      // console.log(cartContainer);
-      // cardContainer.innerHTML = "";
-      cartProducts.forEach((cartProduct) => {
-        cartContainer.innerHTML += `
+
+      localStorage.setItem("cart products", JSON.stringify(cartProducts));
+      showCartItems(cartProducts);
+      // localStorage.productsOnCart = cartProducts;
+      //     //
+      //     // localStorage.ProductsOnCart = cartProduct;
+      // let cartContainer = document.querySelector(".list-group");
+      // //     // console.log(cartContainer);
+      // cartContainer.innerHTML = "";
+      // console.log(cartProducts);
+      // cartProducts.forEach((product) => {
+      //   console.log(product);
+      //   cartContainer.innerHTML += `
+      //         <li class="list-group-item">
+      //           <div class="itm">${product[1]}</div>
+      //           <div class="price">${product[2]}</div>
+      //           <div class="dlt">
+      //             <button onclick="deleteFromCart(${product[0]})" class="btn btn-danger btn-sm float-right delete">
+      //               <i class="fas fa-trash-alt"></i>
+      //             </button>
+      //           </div>
+      //         </li>
+      // `;
+      // });
+      // let totalPrice = cartProducts.reduce(
+      //   (total, price) => total + parseInt(price[2]),
+      //   0
+      // );
+      // document.querySelector(".calc").innerHTML = "";
+      // document.querySelector(
+      //   ".calc"
+      // ).innerHTML = `<div>TOTAL PRICE: R${totalPrice}</div>`;
+    });
+}
+
+function showCartItems(cartProducts) {
+  let cartContainer = document.querySelector(".list-group");
+  //     // console.log(cartContainer);
+  cartContainer.innerHTML = "";
+  console.log(cartProducts);
+  cartProducts.forEach((product) => {
+    console.log(product);
+    cartContainer.innerHTML += `
           <li class="list-group-item">
-            <div class="itm">${cartProduct[1]}</div>
-            <div class="price">${cartProduct[2]}</div>
+            <div class="itm">${product[1]}</div>
+            <div class="price">${product[2]}</div>
             <div class="dlt">
-              <button onclick="deleteFromCart(${cartProduct[0]})" class="btn btn-danger btn-sm float-right delete">
+              <button onclick="deleteFromCart(${product[0]})" class="btn btn-danger btn-sm float-right delete">
                 <i class="fas fa-trash-alt"></i>
               </button>
             </div>
           </li>
   `;
-      });
-      let totalPrice = cartProducts.reduce(
-        (total, price) => total + parseInt(price[2]),
-        0
-      );
-      document.querySelector(".calc").innerHTML = "";
-      document.querySelector(
-        ".calc"
-      ).innerHTML = `<div>TOTAL PRICE: R${totalPrice}</div>`;
-    });
+  });
+
+  let totalPrice = cartProducts.reduce(
+    (total, price) => total + parseInt(price[2]),
+    0
+  );
+  document.querySelector(".calc").innerHTML = "";
+  document.querySelector(
+    ".calc"
+  ).innerHTML = `<div>TOTAL PRICE: R${totalPrice}</div>`;
 }
 
-function deleteFromCart(id) {}
+showCartItems(JSON.parse(localStorage.getItem("cart products")));
 
-function renderCards() {
+function deleteFromCart(id) {
+  let products = JSON.parse(localStorage.getItem("cart products"));
+  let filtered_products = products.filter((product) => product[0] != id);
+
+  localStorage.setItem("cart products", JSON.stringify(filtered_products));
+
+  showCartItems(JSON.parse(localStorage.getItem("cart products")));
+}
+
+function renderCards(products) {
   let productContainer = document.querySelector(".products");
   for (product of products) {
     let card = createCard(product);
@@ -97,7 +143,7 @@ function renderCards() {
   }
 }
 
-renderCards();
+// renderCards();
 
 function filterCards(category) {
   let cards = document.getElementsByClassName("product-item");
--- JS/form.js
+++ JS/form.js
@@ -1,3 +1,32 @@
+let form = document.querySelector("#contact-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let name = document.querySelector("#name").value;
+  let price = document.querySelector("#price").value;
+  let type = document.querySelector("#type").value;
+  let descreption = document.querySelector("#description").value;
+  let image = document.querySelector("#image").value;
+
+  let product = {
+    name,
+    price,
+    type,
+    descreption,
+    image,
+  };
+
+  fetch("https://lit-beach-56409.herokuapp.com/create-product/", {
+    method: "POST",
+    body: JSON.stringify(product),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+});
+
 function createNewProuct() {
   productInfo = [];
   let input = document.querySelector("input");
